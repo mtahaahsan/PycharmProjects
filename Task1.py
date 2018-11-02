dd9a07da7ef13327a8c4ca251cd9c47db39e2af0
Muhammad Ahsan <mtahaahsan@gmail.com>
2018-11-03 00:24:09 +0400

    Testing uploads

--- Task1.py
+++ Task1.py
@@ -1,4 +1,15 @@
-def cadd(x, y):
-    print(x+y)
+def cadd(tups, tups2):
+    first, last = tups
+    first2, last2 = tups2
+    return ((first + first2), (last + last2))
 
-cadd(1,2)
+
+print(cadd((1, 23), (4, 1)))
+
+def cmult(tups, tups2):
+    first, second = tups
+    first2, second2 = tups2
+    return ((first*first2), (second*second2))
+
+print(cmult((1,2), (12,124)))
+
--- Task2.py
+++ Task2.py
@@ -1,4 +1,27 @@
-def cmult(x,y):
-    print(x*y)
+list1 = [1,2,3]
+list2 = [4,5,6]
+list = []
 
-cmult(1,5)
+
+def seqaddi(list1, list2):
+        for x in zip(list1, list2):
+            list.append(sum(x))
+        return list
+
+def seqmulti (list1, list2):
+    for x,y in zip(list1, list2):
+        z=x*y
+        list.append(z)
+    return list
+
+def seqaddr(list1, list2):
+    if not list1:
+        first,*rest = list1
+        first2,*rest2 = list2
+    list.append(first+first2)
+    seqaddr(rest, rest2)
+    return list
+
+
+print(seqaddr(list1,list2))
+
